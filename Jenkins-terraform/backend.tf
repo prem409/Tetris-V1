a947aaa5a1cc5469ac4da2a3f3232feed10dbdaf
prem409 <154398215+prem409@users.noreply.github.com>
2024-02-07 17:22:14 +0530

    Update backend.tf

--- Jenkins-terraform/backend.tf
+++ Jenkins-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "rajinikanths3" # Replace with your actual S3 bucket name
+    bucket = "bucketpremm" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "ap-south-1"
   }
